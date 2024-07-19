fd3506bf0aa858c8a1be3e583ab2e38cc87d4c74
Antoine-ValentinCharpentier <antoinevalentin.charpentier@gmail.com>
2024-07-19 21:44:16 +0200

    :truck: Move the parse url function to utils file

--- scrapper/utils.py
+++ scrapper/utils.py
@@ -1,9 +1,36 @@
+from urllib.parse import urlparse, parse_qsl
 import pandas as pd
 import os
-from typing import List
+from typing import List, Dict
 
 from item import Item
 
+def parse_url(url_research, nbItems=20, page=1, time=None) -> Dict:
+    query_data = urlparse(url_research).query
+    querys = parse_qsl(query_data)
+
+    params = {
+        "search_text": get_param(querys, "search_text", join_with="+"),
+        "catalog_ids": get_param(querys, "catalog[]"),
+        "color_ids": get_param(querys, "color_ids[]"),
+        "brand_ids": get_param(querys, "brand_ids[]"),
+        "size_ids": get_param(querys, "size_ids[]"),
+        "material_ids": get_param(querys, "material_ids[]"),
+        "status_ids": get_param(querys, "status[]"),
+        "country_ids": get_param(querys, "country_ids[]"),
+        "city_ids": get_param(querys, "city_ids[]"),
+        "is_for_swap": get_param(querys, "disposal[]", join_with=",", default="1"),
+        "currency": get_param(querys, "currency"),
+        "price_to": get_param(querys, "price_to"),
+        "price_from": get_param(querys, "price_from"),
+        "page": page,
+        "per_page": nbItems,
+        "order": get_param(querys, "order"),
+        "time": time
+    }
+
+    return params
+    
 def get_param(querys, key, join_with=',', default=''):
     """Extrait les valeurs associées à une clé, les joint avec un séparateur et retourne une chaîne."""
     return join_with.join(map(str, [value for k, value in querys if k == key])) or default
--- scrapper/vinted.py
+++ scrapper/vinted.py
@@ -1,10 +1,9 @@
-from urllib.parse import urlparse, parse_qsl
 from requests.exceptions import HTTPError
 from typing import List, Dict
 
 from requester import Requester
 from item import Item
-from utils import get_param, save_to_csv
+from utils import parse_url, save_to_csv
 
 from  constants import VINTED_API_URL, VINTED_PRODUCTS_ENDPOINT, VINTED_DTOS_ENDPOINT, VINTED_URL_PAGE_CATALOG
 
@@ -26,7 +25,7 @@ class Vinted:
         
         try:
             for page in range(starting_page, ending_page+1):
-                params_request = self.parse_url(url_research, nb_items_page, page, time)
+                params_request = parse_url(url_research, nb_items_page, page, time)
 
                 response = self.requester.get(
                     url=f"{VINTED_API_URL}/{VINTED_PRODUCTS_ENDPOINT}", 
@@ -77,29 +76,3 @@ class Vinted:
                             if sub_sub_catalogs['title'] not in excludes_catalogs_names:
                                 catalogs_ids.append(sub_sub_catalogs['id'])
         return catalogs_ids
-
-    def parse_url(self, url_research, nbItems=20, page=1, time=None) -> Dict:
-        query_data = urlparse(url_research).query
-        querys = parse_qsl(query_data)
-
-        params = {
-            "search_text": get_param(querys, "search_text", join_with="+"),
-            "catalog_ids": get_param(querys, "catalog[]"),
-            "color_ids": get_param(querys, "color_ids[]"),
-            "brand_ids": get_param(querys, "brand_ids[]"),
-            "size_ids": get_param(querys, "size_ids[]"),
-            "material_ids": get_param(querys, "material_ids[]"),
-            "status_ids": get_param(querys, "status[]"),
-            "country_ids": get_param(querys, "country_ids[]"),
-            "city_ids": get_param(querys, "city_ids[]"),
-            "is_for_swap": get_param(querys, "disposal[]", join_with=",", default="1"),
-            "currency": get_param(querys, "currency"),
-            "price_to": get_param(querys, "price_to"),
-            "price_from": get_param(querys, "price_from"),
-            "page": page,
-            "per_page": nbItems,
-            "order": get_param(querys, "order"),
-            "time": time
-        }
-
-        return params
